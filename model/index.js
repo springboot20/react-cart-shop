74af13044d90ab310c99a7cf138a5bf928df7375
Yunus Abbas Opeyemi <opeyemiakanbi328@gmail.com>
2023-05-30 12:17:38 +0100

    model/index.js file changes

--- model/index.js
+++ model/index.js
@@ -1,10 +1,12 @@
 const Product = require("./product.js")
 const User = require("./user.js")
 const RefreshToken = require("./refreshToken.js")
+const Order = require("./order.js")
 
 
 module.exports = {
     Product,
     User,
-    RefreshToken
+    RefreshToken,
+    Order
 }
