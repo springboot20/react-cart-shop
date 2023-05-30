f9174e0672fa3b4733cfeec131d21f780c6c31e9
Yunus Abbas Opeyemi <opeyemiakanbi328@gmail.com>
2023-05-30 14:12:20 +0100

    routes/product.js file changes

--- routes/product.js
+++ routes/product.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 const { createProduct, getAllProducts, getProduct, updateProduct, deleteProduct, getProductsByType } = require("../controller/product.js")
 
-router.post('/create-product', createProduct)
+router.post('/', createProduct)
 router.get('/', getAllProducts)
 router.get("/:type", getProductsByType)
 router.patch('/:id', updateProduct)
