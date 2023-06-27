ab2f64b42999624839a9fecf8d78093c27d5c452
Yunus Abbas Opeyemi <opeyemiakanbi328@gmail.com>
2023-06-27 21:20:28 +0100

    file changes

--- model/user.js
+++ model/user.js
@@ -8,49 +8,40 @@ const userSchema = new Schema(
     firstName: {
       type: String,
       require: true,
-      default: null,
     },
     lastName: {
       type: String,
       require: true,
-      default: null,
     },
     email: {
       type: String,
       require: true,
-      default: null,
       unique: true,
     },
     password: {
       type: String,
       require: true,
-      default: null,
     },
     country: {
       type: String,
       require: true,
-      default: null,
     },
     address: {
       state: {
         type: String,
         require: true,
-        default: null,
       },
       streetAddress: {
         type: String,
         require: true,
-        default: null,
       },
       city: {
         type: String,
         require: true,
-        default: null,
       },
       zipCode: {
         type: Number,
         require: true,
-        default: null,
       },
     },
     isAdmin: {
