decd0f8592ca7051d6425f64b58e3ca13a2df761
Yunus Abbas Opeyemi <opeyemiakanbi328@gmail.com>
2023-07-26 23:22:23 +0100

    file changes and modified

--- src/components/Api/Axios.jsx
+++ src/components/Api/Axios.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 export const Axios = axios.create({
   // baseURL: 'http://localhost:4000/api/v1',
-  baseURL: 'https://cart-product-api.onrender.com',
+  baseURL: 'https://cart-product-api.onrender.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
