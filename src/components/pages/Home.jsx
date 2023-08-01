bbffd01fb53614e39ba0e314bed472d6756d208c
Yunus Abbas Opeyemi <opeyemiakanbi328@gmail.com>
2023-08-01 21:20:01 +0100

    home css style changes

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -5,30 +5,32 @@ import Button from '../components/icon/Button';
 const Home = () => {
   return (
     <Fragment>
-      <section className='h-[60vh] lg:h-[calc(100vh-12rem)] mx-auto max-w-5xl md:max-w-7xl lg:max-w-[90rem] xl:max-w-[95rem] grid grid-cols-1 sm:grid-cols-2 gap-24 place-items-center place-content-center px-10'>
-        <article className='col-span-full sm:col-span-1'>
-          <h1 className='text-gray-900 dark:text-white text-5xl font-extrabold mb-7'>
-            Design Your <br /> Comfort Zone
-          </h1>
-          <p className='text-gray-600 dark:text-white text-lg font-medium mb-7'>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto, at sed omnis corporis doloremque possimus
-            velit! Repudiandae nisi odit, aperiam odio ducimus, obcaecati libero et quia tempora excepturi quis alias?
-          </p>
-          <Button
-            className='uppercase px-7 py-2 rounded-md bg-gray-900 hover:bg-gray-700 dark:bg-white dark:text-gray-800 text-xl font-bold text-white'
-            type='button'>
-            shop now
-          </Button>
-        </article>
-        <article className='col-span-full sm:col-span-1'>
-          <h1 className='text-gray-900 dark:text-white text-5xl font-extrabold mb-7'>
-            Design Your <br /> Comfort Zone
-          </h1>
-          <p className='text-gray-600 dark:text-white text-lg font-medium mb-7'>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto, at sed omnis corporis doloremque possimus
-            velit! Repudiandae nisi odit, aperiam odio ducimus, obcaecati libero et quia tempora excepturi quis alias?
-          </p>
-        </article>
+      <section className='h-[60vh] lg:h-[calc(100vh-12rem)] relative top-24 mb-14'>
+        <div className='h-full mx-auto max-w-5xl md:max-w-7xl lg:max-w-[90rem] xl:max-w-[95rem] grid grid-cols-1 sm:grid-cols-2 gap-24 place-items-center place-content-center px-10'>
+          <article className='col-span-full sm:col-span-1'>
+            <h1 className='text-gray-900 dark:text-white text-5xl font-extrabold mb-7'>
+              Design Your <br /> Comfort Zone
+            </h1>
+            <p className='text-gray-600 dark:text-white text-lg font-medium mb-7'>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto, at sed omnis corporis doloremque possimus
+              velit! Repudiandae nisi odit, aperiam odio ducimus, obcaecati libero et quia tempora excepturi quis alias?
+            </p>
+            <Button
+              className='uppercase px-7 py-2 rounded-md bg-gray-900 hover:bg-gray-700 dark:bg-white dark:text-gray-800 text-xl font-bold text-white'
+              type='button'>
+              shop now
+            </Button>
+          </article>
+          <article className='col-span-full sm:col-span-1'>
+            <h1 className='text-gray-900 dark:text-white text-5xl font-extrabold mb-7'>
+              Design Your <br /> Comfort Zone
+            </h1>
+            <p className='text-gray-600 dark:text-white text-lg font-medium mb-7'>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto, at sed omnis corporis doloremque possimus
+              velit! Repudiandae nisi odit, aperiam odio ducimus, obcaecati libero et quia tempora excepturi quis alias?
+            </p>
+          </article>
+        </div>
       </section>
 
       <section className='bg-gray-300 py-20 mx-auto'>
