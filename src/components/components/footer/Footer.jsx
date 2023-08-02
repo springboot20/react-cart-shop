9a79ec3afe7009e9843fe51c98fb7da562ada139
Yunus Abbas Opeyemi <opeyemiakanbi328@gmail.com>
2023-08-02 15:27:36 +0100

    mobile responsiveness for page added

--- src/components/components/footer/Footer.jsx
+++ src/components/components/footer/Footer.jsx
@@ -18,31 +18,23 @@ const currentYear = new Date().getFullYear();
 
 const Footer = () => {
   return (
-    <footer className='relative w-full bg-white shadow-md dark:bg-gray-800 my-auto'>
+    <footer className='w-full bg-white shadow-md dark:bg-gray-800 my-auto'>
       <div className='mx-auto w-full max-w-5xl md:max-w-7xl lg:max-w-[90rem] px-8 py-3'>
         <div className='grid grid-cols-1 justify-between gap-4 md:grid-cols-2'>
-          <div className='flex items-center justify-center md:justify-start space-x-5'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 576 512'
-              fill='rgb(139 92 246)'
-              className='h-12'>
+          <div className='flex items-start justify-center md:justify-start space-x-5'>
+            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 576 512' fill='rgb(139 92 246)' className='h-12'>
               <path d='M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z' />
             </svg>
-            <h5 className='text-gray-900 dark:text-white text-4xl font-semibold font-ubuntu'>
-              Cart
-            </h5>
+            <h5 className='text-gray-900 dark:text-white text-4xl font-semibold font-ubuntu'>Cart</h5>
           </div>
-          <div className='grid grid-cols-2 justify-between gap-4 md:gap-16'>
+          <div className='flex flex-col md:flex-row justify-center items-center md:items-start gap-4 md:gap-16 mt-8 md:mt-0'>
             {LINKS.map(({ title, items, to }) => (
               <ul key={title}>
-                <Link
-                  to='/'
-                  className='mb-3 font-medium text-gray-900 dark:text-white text-2xl'>
+                <Link to='/' className='mb-3 font-medium text-gray-900 dark:text-white text-3xl'>
                   {title}
                 </Link>
                 {items.map((link, index) => (
-                  <li key={index}>
+                  <li key={index} className='text-center md:text-left'>
                     <Link
                       to={to[index]}
                       className='dark:text-white text-gray-900 py-1.5 transition-colors dark:hover:text-gray-400 text-lg font-medium hover:underline'>
@@ -56,9 +48,7 @@ const Footer = () => {
         </div>
         <div className='mt-12 flex w-full flex-col items-center justify-center border-t border-blue-gray-200 py-4 md:flex-row md:justify-between'>
           <p className='mb-4 text-center font-normal dark:text-white text-gray-900 md:mb-0'>
-            &copy; {currentYear}{' '}
-            <Link to='https://github.com/springboot19'>Springboot19</Link>. All
-            Rights Reserved.
+            &copy; {currentYear} <Link to='https://github.com/springboot19'>Springboot19</Link>. All Rights Reserved.
           </p>
           <div className='flex gap-4 text-blue-gray-900 sm:justify-center'>
             <Link to='https://web.facebook.com/index.php?_rdc=1&_rdr'>
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -5,7 +5,7 @@ import Button from '../components/icon/Button';
 const Home = () => {
   return (
     <Fragment>
-      <section className='h-[60vh] lg:h-[calc(100vh-12rem)] relative top-24 mb-14'>
+      <section className='h-[60vh] lg:h-[calc(100vh-12rem)] relative top-24'>
         <div className='h-full mx-auto max-w-5xl md:max-w-7xl lg:max-w-[90rem] xl:max-w-[95rem] grid grid-cols-1 sm:grid-cols-2 gap-24 place-items-center place-content-center px-6'>
           <article className='col-span-full sm:col-span-1'>
             <h1 className='text-gray-900 dark:text-white text-5xl font-extrabold mb-7'>
@@ -33,12 +33,11 @@ const Home = () => {
         </div>
       </section>
 
-      <section className='bg-gray-300 py-20 mx-auto'>
+      <section className='bg-gray-300 relative top-24 py-8'>
         <header className='w-full text-center'>
           <h2 className='text-gray-700 font-bold italic text-4xl'>Featured Products</h2>
         </header>
-        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-12 w-[90vw] mx-auto mt-12'>
-          <article className='col-span-full bg-blue-gray-900 h-[26rem] md:col-span-1 rounded-md'></article>
+        <div className='mx-auto max-w-5xl md:max-w-7xl lg:max-w-[90rem] xl:max-w-[95rem] grid sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-8 px-10 lg:px-0'>
           <article className='col-span-full bg-blue-gray-900 h-[26rem] md:col-span-1 rounded-md'></article>
           <article className='col-span-full bg-blue-gray-900 h-[26rem] md:col-span-1 rounded-md'></article>
           <article className='col-span-full bg-blue-gray-900 h-[26rem] md:col-span-1 rounded-md'></article>
